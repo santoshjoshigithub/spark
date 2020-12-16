c21aedb6f666f56f759aff543b3d56965c8148e5
Santosh Joshi <kec.santosh.joshi@gmail.com>
2020-12-16 11:47:44 +0800

    updated the comments.

--- rdd/average_friends_by_age.py
+++ rdd/average_friends_by_age.py
@@ -26,16 +26,20 @@ row = sc.textFile("C:/SparkCourse/fakefriends.csv")
 # parse each row and extract the key value pair ithe age_friends RDD (age, number_of_friends)
 age_friends = row.map(parseRow)
 
+
 # for each row, first apply mapValues on each row and then reduceByKey
-# mapValues - 
-# reduceByKey -
+# mapValues - Pass each value in the key-value pair RDD through a map function without changing the keys; this also retains the original RDD’s partitioning.
+# reduceByKey - reduceByKey(func, numPartitions=None, partitionFunc=<function portable_hash>)[source] 
+#               Merge the values for each key using an associative and commutative reduce function.
+#               This will also perform the merging locally on each mapper before sending results to a reducer, similarly to a “combiner” in MapReduce.
+#               Output will be partitioned with numPartitions partitions, or the default parallelism level if numPartitions is not specified. Default partitioner is hash-partition.
 
 totals_by_age = age_friends.mapValues(lambda x: (x,1)).reduceByKey(lambda x,y: (x[0]+y[0],x[1]+y[1]))
+# Note: in the above example, reduceByKey will keep adding all the values of a particular keys until the all the records pertaining to that key are exhausted.
+
 average_by_age = totals_by_age.mapValues(lambda x: x[0] / x[1])
 final_result = average_by_age.collect()
 for result in final_result:
     print(result)
 
 #Important: reduceByKey is faster as it will not create new partitions.
-
-#mention what are actions and what are transformations
